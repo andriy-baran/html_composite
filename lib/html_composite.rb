da7f0a6b9aff3b4392a21a5abf1b6dcd1a15ce13
Andrii Baran <andriy.baran.v@gmail.com>
2021-03-26 21:09:05 +0200

    Init

---
+++ lib/html_composite.rb
@@ -0,0 +1,6 @@
+require "html_composite/version"
+
+module HtmlComposite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/html_composite/version.rb
@@ -0,0 +1,3 @@
+module HtmlComposite
+  VERSION = '0.1.0'.freeze
+end
